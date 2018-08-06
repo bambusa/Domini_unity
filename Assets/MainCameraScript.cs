70a498793f5578a7e6142db7e6272290d8db5591
bambusa <beny-ulms@gmx.de>
2018-08-06 12:47:22 +0200

    Initial commit

---
+++ Assets/MainCameraScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Domini;
+using Domini.Buildings;
+using UnityEngine;
+
+public class MainCameraScript : MonoBehaviour
+{
+    private GameManager _gameManager;
+    
+    // Use this for initialization
+    void Start()
+    {
+        print("Start");
+        _gameManager = GameManager.Instance;
+        _gameManager.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _gameManager.Update();
+        if (Input.GetMouseButtonUp(0))
+        {
+            print("Clicked");
+            _gameManager.BuildingManager.Build(Building.Woodjack);
+        }
+    }
+}
